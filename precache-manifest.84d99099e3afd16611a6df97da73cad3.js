50d748bbc823998b9f9126b2c4904e905e9bb144
Rishi Raj <86799086+RishiRaj11@users.noreply.github.com>
2021-09-05 18:35:38 +0530

    Updates

---
+++ precache-manifest.84d99099e3afd16611a6df97da73cad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd70fb170b31bf233edd30c4873303e",
+    "url": "/Happay_Assignment/index.html"
+  },
+  {
+    "revision": "819bc2080dfca8a3f438",
+    "url": "/Happay_Assignment/static/css/main.46ba7e09.chunk.css"
+  },
+  {
+    "revision": "c0c6dcfaaf5fc268a423",
+    "url": "/Happay_Assignment/static/js/2.647ed51e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Happay_Assignment/static/js/2.647ed51e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819bc2080dfca8a3f438",
+    "url": "/Happay_Assignment/static/js/main.629568f9.chunk.js"
+  },
+  {
+    "revision": "2effdbb893f9c4878dab",
+    "url": "/Happay_Assignment/static/js/runtime-main.1ed081db.js"
+  }
+]);
